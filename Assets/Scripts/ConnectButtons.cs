04d8816456c60978874df6d4e3bedd6f9b85c8f1
Maffia9770 <Maffia9770@gmail.com>
2022-12-16 15:30:20 +0100

    Button

--- Assets/Scripts/ConnectButtons.cs
+++ Assets/Scripts/ConnectButtons.cs
@@ -8,12 +8,15 @@ public class ConnectButtons : MonoBehaviour
 {
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private Button clientJonasBtn;
+    [SerializeField] private Button clientCasperBtn;
     // Start is called before the first frame update
     void Start()
     {
         hostBtn.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
         clientBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
-
+        clientJonasBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+        clientCasperBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
     }
 
     // Update is called once per frame
