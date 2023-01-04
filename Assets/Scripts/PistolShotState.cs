22a2abe9315dee4e5b0b5216b99bf8377bc8033f
Korpo-spec <casper22x@gmail.com>
2023-01-04 15:23:56 +0100

    NetworkAnimator

--- Assets/Scripts/PistolShotState.cs
+++ Assets/Scripts/PistolShotState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "State/Player/ShootGun")]
@@ -7,22 +8,23 @@ public class PistolShotState : State
 {
 
     [SerializeField] private MovementState movementstate;
-    private Animator animator;
-    private static readonly int ShootGun = Animator.StringToHash("ShootGunn");
+    private NetworkAnimator networkAnimator;
+    
+    private static readonly int ShootGun = Animator.StringToHash("ShootGun");
 
     public override void OnEnter(StateController controller)
     {
         base.OnEnter(controller);
-        animator = controller.GetComponent<Animator>();
+        networkAnimator = controller.GetComponent<NetworkAnimator>();
         //controller.transform.Rotate(new Vector3(0,90,0));
-        animator.SetBool(ShootGun, true);
+        networkAnimator.SetTrigger(ShootGun);
     }
 
     public override void UpdateState()
     {
-        if (!animator.IsPlayingState("ShootingGun"))
+        if (!networkAnimator.Animator.IsPlayingState("ShootingGun"))
         {
-            animator.SetBool(ShootGun, false);
+            
             Debug.Log("transition");
             controller.Transistion(movementstate);
         }
