2da0f5fb4354c8b9682a14c4389f2523d5454993
Jonas <jfand125@gmail.com>
2023-06-13 16:43:14 +0200

    Enemy run into you haha

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Entity
+{
+    [SerializeField] private float detectionDistance;
+
+    [SerializeField] private Transform target;
+
+    [SerializeField] private LayerMask mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = new RectTransform();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (target)
+        {
+            transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position, Vector3.up);
+        }
+        else
+        {
+            FindPlayer();
+        }
+    }
+
+    void FindPlayer()
+    {
+        /*Physics.SphereCast(transform.position, detectionDistance, transform.forward,
+            out RaycastHit hitinfo,detectionDistance,mask);
+        target = hitinfo.transform;*/
+        var a = Physics.OverlapSphere(transform.position, detectionDistance, mask);
+        foreach (var col in a)
+        {
+            if (col.gameObject.CompareTag("Player"))
+            {
+                target = col.gameObject.transform;
+            }
+        }
+    }
+}
