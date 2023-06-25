e7492ad08888a29dafdb91aec72ae16daa805ab2
Korpo-spec <casper22x@gmail.com>
2023-06-25 15:37:10 +0200

    Ability changes

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -1,8 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(menuName = "Ability")]
 public class Ability : ScriptableObject
 {
-    
+    public Sprite sprite;
+    public int cost;
+    public State abilityCode;
+
+    public void OnAbilityCast(StateController controller)
+    {
+        controller.AddState(abilityCode);
+    }
 }
